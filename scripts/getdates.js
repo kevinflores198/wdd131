d9c80ae02e2c3aa1b808e0cde5ccf4393d1cb029
kevin lores <kevinfloresg1998@gmail.com>
2026-09-09 18:25:38 -0300

    fixed twice

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,5 +1,16 @@
 const today = new Date();
-currentyear.innerHTML= today.getFullYear();
+currentyear.innerHTML = today.getFullYear();
 
 const lastMod = new Date(document.lastModified);
-lastmodified.innerHTML = lastMod;
+//to cut the last modification in HTML, I used this way that shows a short date, just showing day and time.
+const options = {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: false
+};
+
+lastmodified.innerHTML = "Last Modification: " + lastMod.toLocaleString("en-US", options);
