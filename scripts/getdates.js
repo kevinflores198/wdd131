fe013d2eb10b1c8af72f2d6e80ee82041436f163
kevin lores <kevinfloresg1998@gmail.com>
2026-09-11 15:12:59 -0300

    doing temples.html css

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -2,6 +2,7 @@ const today = new Date();
 currentyear.innerHTML = today.getFullYear();
 
 const lastMod = new Date(document.lastModified);
+
 //to cut the last modification in HTML, I used this way that shows a short date, just showing day and time.
 const options = {
     year: "numeric",
--- scripts/temples.js
+++ scripts/temples.js
@@ -4,10 +4,23 @@ currentyear.innerHTML= today.getFullYear();
 const lastMod = new Date(document.lastModified);
 lastmodified.innerHTML = lastMod;
 
+const options = {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: false
+};
+
+lastmodified.innerHTML = "Last Modification: " + lastMod.toLocaleString("en-US", options);
+
 const main = document.querySelector('.navigation');
-const hamb = document.querySelector('#menu');
+const hamb = document.querySelector('#burger');
 
 hamb.addEventListener('click', () => {
     main.classList.toggle('showing');
     hamb.classList.toggle('active');
 });
+
